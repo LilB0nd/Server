71ccdd553bef2b8027ec07fd439434b923f4278c
joshua121 <Joshua121@schule.bremen.de>
2021-03-16 10:11:03 +0100

    Standart

--- P5/views.py
+++ P5/views.py
@@ -8,25 +8,18 @@ def detail_order(request, order_id):
     order_list = Order.objects.get(Order_ID=order_id)
     template = loader.get_template('P5/detail_order.html')
     dish_of_order = order_list.Dish_list.all()
-    #print("")
-    #print(order_list)
-    #print(dish_of_order)
-    print("")
+
     #print(order_list.quantity_set.all())
     quantity = order_list.quantity_set.get_queryset()
+
     for element in quantity:
+        print(element.amount)
         print(element.Dish.Dish_Name)
-    #print(quantity.Dish.Dish_Name)
-    print("")
-    context = {'detail_order': order_list, 'dish_list': dish_of_order}
-    """
-    dishes = ""
-    for dish in dish_of_order:
-        print(dish)
-        dishes = dishes + " " + str(dish)
-    
-    """
-    print(order_list.Dish_list.all())
+        print(element.Dish.Dish_Price)
+
+
+
+    context = {'dish_list': quantity, "order": order_list}
 
     return HttpResponse(template.render(context, request))
 
