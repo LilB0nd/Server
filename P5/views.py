8bc535ef7dd724ac1a4adf637f87a7cbeddcaabb
joshua121 <Joshua121@schule.bremen.de>
2021-03-18 14:35:24 +0100

    Gerichte anzeigen (sortiert)

--- P5/views.py
+++ P5/views.py
@@ -1,8 +1,7 @@
-from django.shortcuts import render
-from django.template import loader
+from django.shortcuts import render, redirect, reverse
 from django.http import HttpResponse
 from django.views import generic
-from .models import Dish, Order, DishCategory, DishTyp
+from .models import Dish, Order, DishCategory, DishTyp, Quantity, Table
 
 
 class OrderView(generic.ListView):
@@ -54,26 +53,13 @@ def beleg(request):
         'gesamt': gmst
     }
     return render(request, "P5/Rechnungen/belege.html", content)
-"""
-def detail_order(request, order_id):
-    order_list = Order.objects.get(Order_ID=order_id)
-    template = loader.get_template('P5/order/detail_order.html')
-    dish_of_order = order_list.Dish_list.all()
 
-    #print(order_list.quantity_set.all())
-    quantity = order_list.quantity_set.get_queryset()
-
-
-    for element in quantity:
-        print(element.amount)
-        print(element.Dish.Dish_Name)
-        print(element.Dish.Dish_Price)
-"""
 
 class DetailDishView(generic.DetailView):
     template_name = 'P5/dish/detail_dish.html'
     context_object_name = 'dish'
     model = Dish
+
 
 
 class DishView(generic.ListView):
@@ -86,6 +72,11 @@ class DishView(generic.ListView):
         list_category = DishCategory.objects.all()
         list_typ = DishTyp.objects.all()
         dish_list = Dish.objects.order_by('typ__dish_category')
+        for category in list_category:
+            list_typ = category.dishtyp_set.get_queryset()
+            for typ in list_typ:
+                dish_list = typ.dish_set.get_queryset()
+                print(dish_list)
 
         context['list_typ'] = list_typ
         context['dish_list'] = dish_list
@@ -94,7 +85,28 @@ class DishView(generic.ListView):
         return context
 
 
+class DishViewTEST(generic.ListView):
+    template_name = 'P5/dish/test.html'
+    context_object_name = 'dish_list'
+    model = Dish
+
+    def post(self, request, *args, **kwargs):
+        new_order = Order()
+        new_order.table_nr = Table.objects.first()
+        last_order_id = Order.objects.last().ID
+        new_order.id = last_order_id + 1
+        new_order.save()
+
+        for dish in Dish.objects.all():
+            dish_id = dish.ID
+            dish = self.request.POST[str(dish.ID) + ':amount']
+            new_quantity = Quantity()
+            new_quantity.Order = Order.objects.get(ID=new_order.id)
+            new_quantity.Dish = Dish.objects.get(ID=dish_id)
+            new_quantity.amount = dish
+            new_quantity.save()
+
+        return redirect('P5:DetailOrderView', pk=new_order.id)
+
 def index(request):
     return HttpResponse("Yvo kann absolut gar nichts")
-# Create your views here.
-#
