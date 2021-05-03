b5ade7eed2a52960a442ac9c0c98e0885d8b414d
LilB0nd <57658534+LilB0nd@users.noreply.github.com>
2021-05-03 16:48:32 +0200

    Server

--- P5/urls.py
+++ P5/urls.py
@@ -9,10 +9,5 @@ urlpatterns = [url('^$', views.index, name='index'),
 
                url(r'^Speisekarte/$', views.DishView.as_view(), name='dishes'),
                url(r'^cart/(?P<pk>.+)/$', views.CartView.as_view(), name='Cart'),
+               url(r'^staffsite/belege/$', login_required(views.beleg), name='beleg')]
 
-
-               url(r'^staffsite/belege/$', login_required(views.beleg), name='beleg'),
-               url(r'^staffsite/statistics/$', login_required(views.SalesStatisticsView.as_view()), name='SalesView'),
-               url(r'^staffsite/order/$', login_required(views.OrderView.as_view()), name='OrderView'),
-               url(r'^staffsite/order/(?P<pk>.+)/$', login_required(views.DetailOrderView.as_view()), name='DetailOrderView'),
-               ]
