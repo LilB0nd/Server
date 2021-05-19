9bd8dbe4650f4f3ae8ee65e543a7a84fae15c894
PsychzZ <55191856+PsychzZ@users.noreply.github.com>
2021-05-19 12:23:48 +0200

    Index.Html ausgebessert

--- P5/urls.py
+++ P5/urls.py
@@ -5,8 +5,9 @@ from . import views
 
 app_name = "P5"
 
-urlpatterns = [url('^$', views.index, name='index'),
+urlpatterns = [
 
+            url(r'^$', views.index, name='home'),
             url(r'^Speisekarte/$', views.DishView.as_view(), name='dishes'),
             url(r'^cart/(?P<order_id>.+)/$', views.CartView.as_view(), name='Cart'),
             url(r'^Beleg/', views.BelegView.as_view(), name='Beleg'),
